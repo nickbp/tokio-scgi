04968f7ee05e9c9b91175c5be5a8969f8e9bbadb
Nick Parker <nick@nickbp.com>
2019-07-01 22:00:11 +1200

    Improve module-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #![deny(warnings, rust_2018_idioms)]
 
-/// For an SCGI server (: Parses SCGI requests and sends back raw byte responses.
+//! SCGI request codec for Tokio.
+//!
+//! This crate provides codecs for creating and parsing SCGI requests, for use by web servers to query SCGI services and backend services to serve SCGI endpoints.
+//! Working examples are provided for asynchronous SCGI servers and clients. Tests meanwhile provide examples of invoking the codecs directly.
+
+/// For an SCGI server (usually a backend service): Parses SCGI requests and sends back raw byte responses.
 pub mod server;
 
 /// For an SCGI client (usually a web server): Builds SCGI requests and receives raw byte responses.
